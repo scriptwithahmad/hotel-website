a60821e2a7b3cd1b570a9907608706bee4b379f0
iMac <imac@iMacs-iMac.local>
2024-01-24 11:44:36 +0500

    UI done

--- src/pages/api/menu/index.js
+++ src/pages/api/menu/index.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
 
         break;
 
-      case "POST":
+      case "GET":
         const finding = await menuModel.find();
         res.status(200).json({
           success: true,
